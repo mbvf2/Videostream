6facb5f1555c1801d16a33cef673f0fd4543624f
mbvf2 <94884866+mbvf2@users.noreply.github.com>
2021-11-26 02:24:18 +0200

    Add files via upload

--- program/callback.py
+++ program/callback.py
@@ -16,35 +16,36 @@ from config import (
 @Client.on_callback_query(filters.regex("cbstart"))
 async def cbstart(_, query: CallbackQuery):
     await query.edit_message_text(
-        f"""💭 مرحبآ [{query.message.chat.first_name}](tg://user?id={query.message.chat.id}) !**\n
-💭 **انــا بــوت اسمي[{BOT_NAME}](https://t.me/{BOT_USERNAME}) تــخـصـوصـي هـو تـــشغيل الاغـــاني و الـــفــيـدوهـــات فــي الـمـحـدثـات الــصوتـيـه**
+        f"""✨ **Welcome [{query.message.chat.first_name}](tg://user?id={query.message.chat.id}) !**\n
+💭 **[{BOT_NAME}](https://t.me/{BOT_USERNAME}) allows you to play music and video on groups through the new Telegram's video chats!**
 
-💡 ** BY » DEVELOPER MAB ♬
+💡 **Find out all the Bot's commands and how they work by clicking on the » 📚 Commands button!**
 
-**""",
+🔖 **To know how to use this bot, please click on the » ❓ Basic Guide button!**""",
         reply_markup=InlineKeyboardMarkup(
-     
+            [
                 [
                     InlineKeyboardButton(
-                        "➕ ** اضـف الـبـوت لي مـجـمـوعـتـك **➕",
+                        "➕ Add me to your Group ➕",
                         url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
                     )
                 ],
+                [InlineKeyboardButton("❓ Basic Guide", callback_data="cbhowtouse")],
                 [
-                InlineKeyboardButton("الاوامـــــــر♬", callback_data="cbbasic"),
-                InlineKeyboardButton("  الاوامــــــــــر ♬", callback_data="cbcmds")
+                    InlineKeyboardButton("📚 Commands", callback_data="cbcmds"),
+                    InlineKeyboardButton("❤ Donate", url=f"https://t.me/{OWNER_NAME}"),
                 ],
                 [
                     InlineKeyboardButton(
-                        "👥 «⫷ 𝗗 𝗔 𝗩  𝗠 𝗔 𝗕 ⫸»",url="t.me/xO_mab_Ox"
+                        "👥 Official Group", url=f"https://t.me/{GROUP_SUPPORT}"
                     ),
                     InlineKeyboardButton(
-                        "👥 «⫷ 𝐺𝑎𝑚𝑏𝑜𝑙↝♡⫸»",url="t.me/G8_M_L"
+                        "📣 Official Channel", url=f"https://t.me/{UPDATES_CHANNEL}"
                     ),
                 ],
                 [
-                   InlineKeyboardButton(
-                        "**قـــنـــاة الـسـورس**", url="t.me/mabx_2")
+                    InlineKeyboardButton(
+                        "🌐 Source Code", url="https://github.com/levina-lab/video-stream"
                     )
                 ],
             ]
@@ -132,7 +133,7 @@ async def cbadmin(_, query: CallbackQuery):
 
 » /pause - pause the stream
 » /resume - resume the stream
-» /skip -switch to next stream
+» /skip - switch to next stream
 » /stop - stop the streaming
 » /vmute - mute the userbot on voice chat
 » /vunmute - unmute the userbot on voice chat
